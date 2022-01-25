a07ef58ed0e4f337527f4995a8d3355706fbca7d
Kyo/coolboy <85298936+Apoorv-create@users.noreply.github.com>
2022-01-25 20:22:06 +0530

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from PIL import ImageTk, Image
+import random
 root=Tk()
 
 root.title("Endless Dice Game")
@@ -24,4 +25,25 @@ player2_score_label.place(relx = 0.9, rely = 0.4, anchor=CENTER)
 random_dice_number = Label(root, bg = "chocolate1", fg = "white")
 random_dice_number.place(relx = 0.5, rely = 0.5, anchor=CENTER)
 
+player1_score = 0
+def player1():
+    global player1_score 
+    random_no = random.randint(1,6)
+    random_dice_number["text"] = "The Number for Player 1 :" + str(random_no)
+    player1_score = player1_score + random_no
+    player1_score_label["text"] = str(player1_score)
+    
+player1_score_btn = Button(root, image = img, command = player1)
+player1_score_btn.place(relx = 0.1, rely = 0.6, anchor=CENTER)
+
+player2_score = 0
+def player2():
+    global player2_score
+    random_no2 = random.randint(1,6)
+    random_dice_number["text"]  = "The Number for Player 2 :" + str(random_no2)
+    player2_score = player2_score + random_no2
+    player2_score_label["text"] = str(player2_score)
+
+player2_score_btn = Button(root, image = img, command = player2)
+player2_score_btn.place(relx = 0.9, rely = 0.6, anchor=CENTER)
 root.mainloop()
